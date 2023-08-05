59f5391ae83b21a367cc659a06b2cc4861fe4ea0
MichaelSandor <msandor@fieldwire.com>
2023-08-04 21:55:20 -0700

    Use library for GA.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,26 +2,17 @@ import { Layout } from "@/components";
 
 import "@/styles/globals.css";
 
-import Script from "next/script";
-
 import type { AppProps } from "next/app";
+
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main>
       <Layout>
+        <GoogleAnalytics gaMeasurementId="G-2NW1Y5GY0F" trackPageViews />
         <Component {...pageProps} />
       </Layout>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-2NW1Y5GY0F" />
-      <Script id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', 'G-2NW1Y5GY0F');
-        `}
-      </Script>
     </main>
   );
 }
