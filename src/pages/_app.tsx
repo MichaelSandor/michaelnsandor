54bbf49ce07aea446c547a147285536bc23bff12
MichaelSandor <msandor@fieldwire.com>
2023-08-04 21:42:56 -0700

    Put script in app instead.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import { Layout } from "@/components";
 
 import "@/styles/globals.css";
 
+import Script from "next/script";
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -10,6 +12,16 @@ export default function App({ Component, pageProps }: AppProps) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-2NW1Y5GY0F" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-2NW1Y5GY0F');
+        `}
+      </Script>
     </main>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <Script src="https://www.googletagmanager.com/gtag/js?id=G-2NW1Y5GY0F" />
-        <Script id="google-analytics">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', 'G-2NW1Y5GY0F');
-        `}
-        </Script>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
