adf0d877ded28bc867a6f470a218ac2d8a757ed0
MichaelSandor <msandor@fieldwire.com>
2023-08-08 19:50:04 -0700

    Fix Google Analytics.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,3 @@
-import { useEffect } from "react";
-
-import { useRouter } from "next/router";
-
-import Script from "next/script";
-
 import { Layout } from "@/components";
 
 import "@/styles/globals.css";
@@ -11,29 +5,8 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (process.env.NEXT_PUBLIC_ENV === "production") {
-      (window as any).gtag("config", "G-2NW1Y5GY0F", {
-        page_title: router.pathname,
-        page_location: `https://michaelnsandor.com${router.pathname}`,
-      });
-    }
-  }, [router.pathname]);
-
   return (
     <main>
-      <>
-        <Script src="https://www.googletagmanager.com/gtag/js?id=G-2NW1Y5GY0F" />
-        <Script id="google-analytics">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){window.dataLayer.push(arguments);}
-            gtag('js', new Date());
-          `}
-        </Script>
-      </>
       <Layout>
         <Component {...pageProps} />
       </Layout>
