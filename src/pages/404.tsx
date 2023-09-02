087273f8a97a0122cc685dc1fcc41721acba7faf
MichaelSandor <msandor@fieldwire.com>
2023-09-02 11:11:46 -0700

    Simplify 404 page.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,7 @@
 import { InternalLink } from "@/components";
 
 function Custom404() {
-  return (
-    <div className="flex flex-1 flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">404</h1>
-      <InternalLink href="/" label="Home" />
-    </div>
-  );
+  return <p>404</p>;
 }
 
 export default Custom404;
