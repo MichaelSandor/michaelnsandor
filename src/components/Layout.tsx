e3caa5cd6f9719a1578f1e031fcaab2e24d3f0e5
MichaelSandor <msandor@fieldwire.com>
2023-09-02 11:11:31 -0700

    Refactor navbar using new Link component behavior. Also make all text text-gray-950, set padding to p-16, and give mb-16.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,13 +10,16 @@ interface LayoutProps {
 
 function Layout(props: LayoutProps) {
   return (
-    <div className="flex min-h-screen flex-col p-8">
-      <nav className="flex items-center justify-end gap-4">
-        <InternalLink href="/" label="Home" />
-        {/* prettier-ignore */}
-        <ExternalLink href="https://github.com/MichaelSandor" label="GitHub" />
-        {/* prettier-ignore */}
-        <ExternalLink href="https://www.linkedin.com/in/sandormichael/" label="LinkedIn" />
+    <div className="text-gray-950 min-h-screen p-16">
+      <nav className="mb-16 flex items-center gap-4">
+        <InternalLink href="/">home</InternalLink>
+        <InternalLink href="/blog/">blog</InternalLink>
+        <ExternalLink href="https://twitter.com/michaelnsandor/">
+          twitter
+        </ExternalLink>
+        <ExternalLink href="https://github.com/MichaelSandor/">
+          github
+        </ExternalLink>
       </nav>
       {props.children}
     </div>
