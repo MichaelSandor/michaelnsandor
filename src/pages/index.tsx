28836f16841504c71e54fb536a0c0b4c4ec2cb4c
MichaelSandor <msandor@fieldwire.com>
2023-09-02 11:12:59 -0700

    Refactor home page using new Link component structure and simplify.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,25 +12,23 @@ function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex flex-1 flex-col items-center justify-center">
-        <div className="flex flex-col gap-4">
-          <h1 className="text-4xl font-bold">Hello!</h1>
-          <p className="text-lg">
-            My name is Michael Sandor and I am an engineer at{" "}
-            <ExternalLink href="https://www.fieldwire.com/" label="Fieldwire" />
-            . I am also working on{" "}
-            <ExternalLink
-              href="https://www.tech-resume.com/"
-              label="tech-resume.com"
-            />{" "}
-            and{" "}
-            <ExternalLink
-              href="https://www.rentdiscountcalculator.com/"
-              label="rentdiscountcalculator.com"
-            />
-            .
-          </p>
-        </div>
+      <div className="flex flex-col gap-2">
+        <h1 className="text-2xl font-bold">hello!</h1>
+        <p>
+          My name is Michael Sandor and I am an engineer at{" "}
+          <ExternalLink href="https://www.fieldwire.com/">
+            Fieldwire
+          </ExternalLink>
+          . I am also working on{" "}
+          <ExternalLink href="https://www.tech-resume.com/">
+            tech-resume.com
+          </ExternalLink>{" "}
+          and{" "}
+          <ExternalLink href="https://www.rentdiscountcalculator.com/">
+            rentdiscountcalculator.com
+          </ExternalLink>
+          .
+        </p>
       </div>
     </>
   );
